05fd65bfe270e04a2cd5aa9e5ae00acfe4dca768
Marcus Colle <marcusvcolle@gmail.com>
2024-07-28 17:50:26 +0100

    Added fetch currency command.

---
+++ app/Console/Commands/FetchCurrencyExchangeRates.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\StoreCurrencyExchangeRates;
+use Illuminate\Console\Command;
+
+class FetchCurrencyExchangeRates extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:currency';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command fetches the latest currency exchange rates from the API and stores them in the database.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //command created to dispatch the job manually from the console for dev test or debugging and trigger the job on task schedule.
+
+        StoreCurrencyExchangeRates::dispatch();
+        $this->info('FetchExchangeRatesJob dispatched.');
+    }
+}
